5d318603cbad2a784f304f0e47fae68ee98251b4
Sibasish Mohanty <smsibasish@gmail.com>
2020-07-15 20:56:28 +0530

    components setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
-import Link from 'next/link';
+import Navbar from '../components/Navbar';
+import Searchbox from '../components/Searchbox';
+import Select from '../components/Select';
 
 const Home = () => (
 	<div>
@@ -7,28 +9,14 @@ const Home = () => (
 			<title>Home</title>
 			<link rel='icon' type='image/svg+xml' href='/favicon.svg' />
 		</Head>
-		<nav className='flex items-center justify-between py-4 px-8 border-b-2 border-gray-300 bg-white'>
-			<Link href='/'>
-				<a className='font-bold tracking-wide text-2xl'>
-					Where in the world?
-				</a>
-			</Link>
-			<div className='flex'>
-				<div className='text-blue-600 font-bold text-center'>
-					<span>Start a new group</span>
-					<br />
-					<span>50% off</span>
-				</div>
-				<div className='border-r border-gray-300 mx-4'></div>
-				<div className='flex items-center'>
-					<ul className='flex items-center space-x-3 text-center text-gray-700 text-sm'>
-						<li>Explore</li>
-						<li>Messages</li>
-						<li>Notifications</li>
-					</ul>
-				</div>
+		<Navbar />
+		<section className='p-4 md:p-8 lg:px-16'>
+			<div className='flex justify-between'>
+				<Searchbox />
+				<Select />
 			</div>
-		</nav>
+			<main></main>
+		</section>
 	</div>
 );
 
